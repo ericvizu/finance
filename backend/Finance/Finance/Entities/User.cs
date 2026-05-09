d58f0bfa21e6c578a69412c49f75919173ad4129
Eric Vizu <eric_vizu@hotmail.com>
2026-05-08 22:49:06 -0300

    feat: it begins

---
+++ backend/Finance/Finance/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Finance.Entities;
+
+public class User
+{
+    private string id; // PK
+    private string name; // Nome
+    private string email; // Email
+    private string password_hash; // Hash da senha
+    private DateTime created_at; // Quando foi criado
+    private DateTime updated_at; // Ultima vez atualizado
+}
---
+++ backend/Finance/Finance/Program.cs
@@ -0,0 +1,9 @@
+namespace Finance; 
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, Mundo!");
+    }
+}
