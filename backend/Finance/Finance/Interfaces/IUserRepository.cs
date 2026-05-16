6f9cceb43abeefbb013fc52e99ab1ff362683757
Eric Vizu <eric_vizu@hotmail.com>
2026-05-16 18:32:35 -0300

    feat: user repository interface created

---
+++ backend/Finance/Finance/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Finance.Entities;
+
+namespace Finance.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsync(Guid id);
+    Task<User?> GetByEmailAsync(string email);
+    Task AddAsync(User user);
+    Task<bool> ExistsByEmailAsync(string email);
+    
+    Task<bool> SaveChangesAsync();
+}
